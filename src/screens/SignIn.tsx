c99f12e17aad0430bc4b449156986175c3fd37c0
Danilo Vaz <danilovaz1305@gmail.com>
2023-12-18 15:50:16 -0300

    Add logo-marca e component de input

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,16 +1,38 @@
-import {VStack, Image} from 'native-base';
+import {VStack, Image, Text, Center, Heading} from 'native-base';
 
 import BackgroundImg from '@assets/background.png';
+import LogoSvg from '@assets/logoreal.svg';
+import {Input} from '@components/Input';
 
 export function SignIn() {
   return (
-    <VStack flex={1} bg="gray.700">
+    <VStack flex={1} bg="gray.600" px={10}>
       <Image
         source={BackgroundImg}
         alt="Pessoas treinando"
         resizeMode="contain"
         position="absolute"
       />
+      <Center my={20}>
+        <LogoSvg />
+
+        <Text color="gray.100" fontSize="sm">
+          Treine sua mente e o seu corpo
+        </Text>
+      </Center>
+
+      <Center>
+        <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
+          Acesse sua conta
+        </Heading>
+
+        <Input
+          placeholder="E-mail"
+          keyboardType="email-address"
+          autoCapitalize="none"
+        />
+        <Input placeholder="Senha" secureTextEntry />
+      </Center>
     </VStack>
   );
 }
