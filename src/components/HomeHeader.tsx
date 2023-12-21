795bbea623f81ac15e4b22ab7d9de0eef7ec7da3
Danilo Vaz <danilovaz1305@gmail.com>
2023-12-21 13:00:22 -0300

    add componente UserPhoto

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,11 +1,22 @@
 import {HStack, Heading, Text, VStack} from 'native-base';
+import {UserPhoto} from './UserPhoto';
 
 export function HomeHeader() {
   return (
-    <HStack>
+    <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
+      <UserPhoto
+        source={{uri: 'https://github.com/danilovaz13.png'}}
+        alt="Imagem do usúario"
+        size={16}
+        mr={4}
+      />
       <VStack>
-        <Text color="gray.100">Olá</Text>
-        <Heading color="gray.100">Danilo</Heading>
+        <Text color="gray.100" fontSize="md">
+          Olá
+        </Text>
+        <Heading color="gray.100" fontSize="md">
+          Danilo
+        </Heading>
       </VStack>
     </HStack>
   );
