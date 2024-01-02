04bb4cf1645680174f4ea5c6ade211a04f6815cd
Danilo Vaz <danilovaz1305@gmail.com>
2024-01-02 14:12:39 -0300

    cadastrando usuário com o method POST

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -47,13 +47,19 @@ export function SignUp() {
     navigation.goBack();
   }
 
-  function handleSignUp({
-    name,
-    email,
-    password,
-    password_confirm,
-  }: FormDataProps) {
-    console.log({name, email, password, password_confirm});
+  function handleSignUp({name, email, password}: FormDataProps) {
+    fetch('http://192.168.3.9:3333/users', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name,
+        email,
+        password,
+      }),
+    });
   }
 
   return (
