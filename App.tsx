2f062a08ddb312dc956c0055c560c842e46b26d4
Danilo Vaz <danilovaz1305@gmail.com>
2024-01-03 15:15:47 -0300

    criando um contexto

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import {StatusBar} from 'react-native';
-import {Box, Text, NativeBaseProvider} from 'native-base';
+import {NativeBaseProvider} from 'native-base';
 
 import {THEME} from './src/theme';
+
 import {Loading} from '@components/Loading';
 import {Routes} from './src/routes';
+
+import {AuthContext} from '@contexts/AuthContext';
 // import {Home} from '@screens/Home';
 
 const test = 'testeando';
@@ -17,7 +20,15 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {test ? <Routes /> : <Loading />}
+      <AuthContext.Provider
+        value={{
+          id: '1',
+          name: 'Danilo',
+          email: 'danilo@gmail.com',
+          avatar: 'danilo.png',
+        }}>
+        {test ? <Routes /> : <Loading />}
+      </AuthContext.Provider>
     </NativeBaseProvider>
   );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,17 @@
+import {useContext} from 'react';
 import {useTheme, Box} from 'native-base';
 import {NavigationContainer, DefaultTheme} from '@react-navigation/native';
+
 import {AuthRoutes} from './auth.routes';
 import {AppRoutes} from './app.routes';
 
+import {AuthContext} from '@contexts/AuthContext';
+
 export function Routes() {
   const {colors} = useTheme();
+
+  const contextoData = useContext(AuthContext);
+  console.log(contextoData);
 
   const theme = DefaultTheme;
   theme.colors.background = colors.gray[600];
